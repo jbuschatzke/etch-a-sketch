4d193e02b605f409f09f6d7b60759ce0259cbfb1
Joey <joezzles@gmail.com>
2023-09-06 16:01:53 -0700

    Listen for mouseover event to add a class that will change box background color

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const container = document.querySelector('.container');
 console.log(container);
 
-// create 32 divs and put inside container
-for (i = 0; i < 256; i++){
-    box = document.createElement('div');
+
+for (i = 0; i < 256; i++){                    // create 256 divs and put inside container
+    let box = document.createElement('div');
     box.classList.add('box');
-    box.textContent = `${i + 1}`;
     container.appendChild(box);
+    box.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
+        box.classList.add('sketch');
+    }
+    box.textContent = `${i + 1}`;
 }
-
-
-console.log(box);
