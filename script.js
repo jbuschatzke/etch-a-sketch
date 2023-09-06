55edf658f3c42ec2b98c3f36b19698adca061e61
Joey <joezzles@gmail.com>
2023-09-06 16:50:08 -0700

    Reiterate grid creation to be input flexible and intilialize button to create a new game

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const container = document.querySelector('.container');
-console.log(container);
+let gridDimension = 256;
+createGrid(gridDimension);
 
-
-for (i = 0; i < 256; i++){                    // create 256 divs and put inside container
-    let box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
-    box.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
-        box.classList.add('sketch');
-    }
-    box.textContent = `${i + 1}`;
+function createGrid(gridDimension){
+    for (i = 0; i < gridDimension; i++){                    // create x divs and put inside container
+        let box = document.createElement('div');
+        box.classList.add('box');
+        container.appendChild(box);
+        box.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
+            box.classList.add('sketch');
+        }
+        box.textContent = `${i + 1}`;
+    };
 }
+
+function newGame() {
+let grid = +prompt("Create a grid of up to 100 squares", "0-100");
+gridDimension = grid * grid;
+if (grid <= 100 && grid > 0){
+    createGrid(gridDimension);
+} else if (isNaN(grid)){
+    alert("This is not a number. Please enter a number between 1 and 100");
+    newGame();
+} else {
+    alert("This is not a valid number. Please enter a number between 1 and 100");
+}
+};
+
+
+// for (i = 0; i < 256; i++){                    // create 256 divs and put inside container
+//     let box = document.createElement('div');
+//     box.classList.add('box');
+//     container.appendChild(box);
+//     box.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
+//         box.classList.add('sketch');
+//     }
+//     box.textContent = `${i + 1}`;
+// };
+
