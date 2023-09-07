0360ede93dbfe73cb7839ce27cb5a204b1af583e
Joey <joezzles@gmail.com>
2023-09-07 14:24:53 -0700

    Clear grid of currently placed divs and place user input

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function newGame() {
 let grid = +prompt("Create a grid of up to 100 squares", "0-100");
 gridDimension = grid * grid;
 if (grid <= 100 && grid > 0){
+    document.querySelectorAll('.box').forEach(e => e.remove());     //grab each currently placed div and remove
     createGrid(gridDimension);
 } else if (isNaN(grid)){
     alert("This is not a number. Please enter a number between 1 and 100");
@@ -26,15 +27,3 @@ if (grid <= 100 && grid > 0){
     alert("This is not a valid number. Please enter a number between 1 and 100");
 }
 };
-
-
-// for (i = 0; i < 256; i++){                    // create 256 divs and put inside container
-//     let box = document.createElement('div');
-//     box.classList.add('box');
-//     container.appendChild(box);
-//     box.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
-//         box.classList.add('sketch');
-//     }
-//     box.textContent = `${i + 1}`;
-// };
-
