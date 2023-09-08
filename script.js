09750c3ba01a1722f3b3b09764017a70c9141e8e
Joey <joezzles@gmail.com>
2023-09-07 17:36:53 -0700

    Rework grid formula to create rows of divs

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const container = document.querySelector('.container');
-let gridDimension = 256;
-createGrid(gridDimension);
+let gridRow = 16;
+let gridDivs = 16;
+createGrid(gridRow, gridDivs);
 
-function createGrid(gridDimension){
-    for (i = 0; i < gridDimension; i++){                    // create x divs and put inside container
-        let box = document.createElement('div');
-        box.classList.add('box');
-        container.appendChild(box);
-        box.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
-            box.classList.add('sketch');
+function createGrid(gridRow, gridDivs){
+    for (i = 0; i < gridRow; i++){                    // create x divs and put inside container
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (j = 0; j < gridDivs; j++){
+            let squares = document.createElement('div');
+            squares.classList.add('squares');
+            row.appendChild(squares);
+            squares.onmouseover = () => {                 // add new "sketch" class to any box that is moused over
+                squares.classList.add('sketch');
+            }
         }
-        box.textContent = `${i + 1}`;
+        // box.textContent = `${i + 1}`;
     };
 }
 
 function newGame() {
 let grid = +prompt("Create a grid of up to 100 squares", "0-100");
-gridDimension = grid * grid;
+gridRow = grid;
+gridDivs = grid;
 if (grid <= 100 && grid > 0){
-    document.querySelectorAll('.box').forEach(e => e.remove());     //grab each currently placed div and remove
-    createGrid(gridDimension);
+    document.querySelectorAll('.squares').forEach(e => e.remove());
+    document.querySelectorAll('.row').forEach(e => e.remove());     //grab each currently placed div and remove
+    createGrid(gridRow, gridDivs);
 } else if (isNaN(grid)){
     alert("This is not a number. Please enter a number between 1 and 100");
     newGame();
